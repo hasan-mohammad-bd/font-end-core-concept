35abdd64a93fd0cd6907319becdcd8a6d65fc6ac
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-03-15 09:37:34 +0600

    routing

---
+++ 44-5-routing.js
@@ -0,0 +1,7 @@
+/*
+Routing:
+===========================
+routing change the path with with an unique address with this address, routing determine what to display to the user. 
+
+to change the routing , write in console: location.hash = "#routing area"
+*/
