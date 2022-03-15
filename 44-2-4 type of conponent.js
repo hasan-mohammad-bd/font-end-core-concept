367061cb55754cbcc42feb6d3d5a3caa642174c5
hasan-mohammad-bd <hasan.md.east@gmail.com>
2022-03-15 07:13:44 +0600

    4 types of component

---
+++ 44-2-4 type of conponent.js
@@ -0,0 +1,14 @@
+/* 
+4 types web component component.
+======================================
+
+component
+======================================
+
+component can be identified 4 ways.
+1. meal DB, or youtube thumbnail are looks(structure.) same but different in data. this similar in loop, but different in data(card) is kind of web component.
+2. container: contained by component. collection of similar looks component.
+3. no pattern, but breakdown for working purpose.
+4. stand alone component(unique)
+
+*/
